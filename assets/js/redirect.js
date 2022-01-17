56c5bc7940a9d126972330fcb5a0eb43ea360648
JacksonMeadeUNC <meade24j@ad.unc.edu>
2022-01-17 16:44:45 -0500

    cmon kill it

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,4 +1,4 @@
 if (window.sessionStorage.getItem("Cleared") != "1") {
     document.title = "Leave Now, Not Authorized";
-    document.getRootNode.body.innerHTML = "";
+    document.body.innerHTML = "";
 }
