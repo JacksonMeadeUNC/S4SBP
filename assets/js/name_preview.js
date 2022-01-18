c90a4159fcb4f53ea5cf65ea99a3813f6563862d
JacksonMeadeUNC <meade24j@ad.unc.edu>
2022-01-18 03:53:45 -0500

    step it up

--- assets/js/name_preview.js
+++ assets/js/name_preview.js
@@ -3,6 +3,7 @@ var people = document.getElementsByClassName("item");
 var title = document.getElementById('personal-title');
 var nomme = document.getElementById('personal-name');
 var content = document.getElementById('personal-content');
+var content2 = document.getElementById('personal-interests');
 
 Array.prototype.filter.call(people, person => {
     if (!person.classList.contains('spacer') && !person.classList.contains('empty') && !person.classList.contains('explainer')) {
@@ -15,6 +16,7 @@ Array.prototype.filter.call(people, person => {
             title.innerHTML = "Welcome!";
             nomme.innerHTML = "MEET THE ROBINSON TEAM";
             content.innerHTML = "Hover over a profile to learn about us!";
+            content2.innerHTML = "";
         })
     };
 });
