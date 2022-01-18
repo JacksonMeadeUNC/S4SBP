a50015bfd20bb3893cd4e1920b9a94b56337f834
JacksonMeadeUNC <meade24j@ad.unc.edu>
2022-01-18 04:15:06 -0500

    add for mobile

--- assets/js/name_preview.js
+++ assets/js/name_preview.js
@@ -12,12 +12,22 @@ Array.prototype.filter.call(people, person => {
             nomme.innerHTML = person.getAttribute("name-for");
             content.innerHTML = person.getAttribute("content-for");
             content2.innerHTML = person.getAttribute("secondary-content-for");
-        })
+        });
         person.addEventListener('mouseleave', e => {
             title.innerHTML = "Welcome!";
             nomme.innerHTML = "MEET THE ROBINSON TEAM";
             content.innerHTML = "Hover over a profile to learn about us!";
             content2.innerHTML = "";
-        })
+        });
+        person.nextSibling.innerHTML = `						<div class="container">
+        <h4 class="card_subtitle style-medium mb-2 style-fonts-style display-4">${person.getAttribute("title-for")}</h4>
+      <h3 class="style-section-subtitle style-fonts-style display-2" style="text-transform: uppercase;">${person.getAttribute("name-for")}</h3>
+      <p class="style-text style-fonts-style display-4">
+      ${person.getAttribute("content-for")}
+    </p>
+    <p class="style-text style-fonts-style display-4">
+      <span id="personal-interests" style="opacity:  0.8">${person.getAttribute("secondary-content-for")}</span>
+  </p>
+          </div>`;
     };
 });
